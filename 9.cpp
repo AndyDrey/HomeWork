4010c48fafbff8b2b08cd329791d54ed4cde94cc
Andy Yarmolenko <andy.yarmol@gmail.com>
2016-12-22 13:15:39 +0200

    9 with changes

--- 9.cpp
+++ 9.cpp
@@ -5,13 +5,13 @@ using namespace std;
 void Shifr(char *c, char *s){
 	for (int i = 0; i < strlen(c); i++)
 	{
-		cout << *((((strchr(s, c[i]) - s) + 5) % 16) +s);
+		cout << *((((strchr(s, c[i]) - s) + 5) % strlen(s)) +s);
 	}
 }
 
 int main(){
-	char c[20];
-	char shifr[17] = "abcdefgh123456-0";
+	char c[20] = {};
+	char shifr[] = "abcdefgh123456-0";
 	cin >> c;
 	Shifr(c, shifr);
 	return 0;
