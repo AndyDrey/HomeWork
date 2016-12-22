20a5dd7a40d5f0b01d16a350b9b78f0c46e534af
Andy Yarmolenko <andy.yarmol@gmail.com>
2016-12-22 13:36:58 +0200

    15 with changes

--- 15.cpp
+++ 15.cpp
@@ -5,40 +5,54 @@ using namespace std;
 int main(){
 	int n,m;
 	cin >> n >> m;
-	//создание двумерного массива
+
+	//creating 1st array
 	int **Arr = new int*[n];
 	for (int i = 0; i < n; i++)
 	{
 		Arr[i] = new int[m];
 	}
-	//создание второго двумерного массива
+
+	//creating 2nd array
 	int **newArr = new int*[m];
 	for (int i = 0; i < m; i++)
 	{
 		newArr[i] = new int[n];
 	}
 
-	//заполнение
+	//fullfill
 	for (int i = 0; i < n; i++)
 	{
 		for (int j = 0; j < m; j++){
 			cin >> Arr[i][j];
 		}
 	}
-	//поворот на 90 градусов по часовой стрелке
+
+	//90dgr rotation
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++){
 			newArr[i][n-j-1] = Arr[j][i];
 		}
 	}
-	//вывод
+
+	//output
 	for (int i = 0; i < m; i++)
 	{
 		for (int j = 0; j < n; j++){
 			cout << newArr[i][j] << " ";
 		}
 		cout << endl;
+	}
+
+	// deleting Arrays
+	for (int i = 0; i < n; i++)
+	{
+		delete[] Arr[i];
+	}
+	for (int i = 0; i < m; i++)
+	{
+		delete[] newArr[i];
 	}
 
 	return 0;
