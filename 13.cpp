8089675c32624c45fe8282213534830c355d0164
Andy Yarmolenko <andy.yarmol@gmail.com>
2016-12-22 13:25:19 +0200

    13 with changes

--- 13.cpp
+++ 13.cpp
@@ -2,29 +2,30 @@
 #include <iostream>
 #include <ctime>
 using namespace std;
-void RandAndShow(int *p){
-	for (int i = 0; i < 50; i++){
-		int x = rand() % 50;
+void RandAndShow(int *p, const int aSize){
+	for (int i = 0; i < aSize; i++){
+		int x = rand() % aSize;
 		int tmp = p[i];
 		p[i] = p[x];
 		p[x] = tmp;
 	}
-	for (int i = 0; i < 50; i++){
+	for (int i = 0; i < aSize; i++){
 		cout << p[i] << " ";
 	}
 }
 
 int main() {
 	srand(time(0));
-	int array[50];
+	const int aSize = 50;
+	int array[aSize];
 	for (int i = 0, j = 1; j < 100; i++, j += 2){
 		array[i] = j;
 	}
-	for (int i = 0; i < 50; i++){
+	for (int i = 0; i < aSize; i++){
 		cout << array[i] << " ";
 	}
 	cout << endl;
 	int *p = array;
-	RandAndShow(p);
+	RandAndShow(p, aSize);
 	return 0;
 }
