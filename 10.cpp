19ca91ae68466d7ccafc58744bd870c803f25afa
Andy Yarmolenko <andy.yarmol@gmail.com>
2016-12-25 13:28:02 +0200

    10task with changes

--- 10.cpp
+++ 10.cpp
@@ -1,26 +1,26 @@
 #include <iostream>
 #include <string>
+
 using namespace std;
 
-
-void toUpper(string s){
+void toUpper(string &s){
 	s[0] = toupper(s[0]);
 	for (int i = 0; i < s.length(); i++) {
-    	if(s[i] == ' '){
-    		cout << s[i];
-    		s[i+1] = toupper(s[i+1]);
-    	}
-    	else cout << s[i];
+		if (s[i] == ' '){
+			cout << s[i];
+			s[i + 1] = toupper(s[i + 1]);
+		}
+		else cout << s[i];
 	}
-	}
-	
-	
+}
+
+
 int main() {
-    string s;
+	string s;
 	getline(cin, s);
-	if(!s.empty()){
+	if (!s.empty()){
 		toUpper(s);
 	}
 	else cout << "Your string is empty!!!";
-  return 0;
+	return 0;
 }
