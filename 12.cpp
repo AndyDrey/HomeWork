62edbcd5720245d633a165640b1e23482d26d738
Andy Yarmolenko <andy.yarmol@gmail.com>
2016-12-25 13:32:39 +0200

    12 with changes

--- 12.cpp
+++ 12.cpp
@@ -1,23 +1,28 @@
 #include <iostream>
 using namespace std;
 
-void SwapAndShow(int *p){
-		for(int i = 0; i < 12; i+=2){
-			int tmp = p[i];
-			p[i] = p[i+1];
-			p[i+1] = tmp;
-		}
-		for(int i = 0; i < 12; i++){
-			cout << p[i] << " ";
-		}
+void SwapAndShow(int *p)
+{
+	for (int i = 0; i < 12; i += 2)
+	{
+		swap(p[i], p[i + 1]);
 	}
+	for (int i = 0; i < 12; i++)
+	{
+		cout << p[i] << " ";
+	}
+}
 int main() {
-	
+
 	int *p;
 	p = new int[12];
-	for(int i = 0; i < 12; i++){
+	for (int i = 0; i < 12; i++)
+	{
 		cin >> p[i];
 	}
 	SwapAndShow(p);
+	
+	delete[] p;
+
 	return 0;
 }
