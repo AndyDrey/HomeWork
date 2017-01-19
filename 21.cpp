84f6adeac2ce83091280d1c4db854f3ea8ce0bcc
Andy Yarmolenko <andy.yarmol@gmail.com>
2017-01-19 17:40:52 +0200

    21 with changes

--- 21.cpp
+++ 21.cpp
@@ -4,66 +4,63 @@ using namespace std;
 
 class Shape
 {
-	double x;
-	double y;
+	double m_x;
+	double m_y;
 public:
-	Shape(double xVal, double yVal){
-		x = xVal;
-		y = yVal;
+	Shape(double xVal, double yVal):m_x(xVal), m_y(yVal){
 	};
-	virtual double square() = 0;
+	virtual double square() const = 0;
 };
 
 class Rectangle : public Shape
 {
-	double width;
-	double height;
+	double m_width;
+	double m_height;
 public:
-	Rectangle(double x, double y, double width, double height): Shape(x,y){
-		this->width = width;
-		this->height = height;
+	Rectangle(double x, double y, double width, double height): Shape(x,y), m_width(width), m_height(height){
+
 	}
-	double square() override
+	double square() const override
 	{
-		return width*height;
+		return m_width*m_height;
 	};
 };
 
 class Triangle : public Shape
 {
-	double base;
-	double height;
+	double m_base;
+	double m_height;
 public:
-	Triangle(double x, double y, double base, double height): Shape(x, y){
-		this->base = base;
-		this->height = height;
+	Triangle(double x, double y, double base, double height): Shape(x, y), m_base(base), m_height(height){
 	}
-	double square() override
+	double square() const override
 	{
-		return base * height / 2;
+		return m_base * m_height / 2;
 	};
 };
 
 class Circle : public Shape
 {
-	double radius;
-	double pi = 3.14;
+	double m_radius;
+	const double m_pi = 3.14;
 public:
-	Circle(double x, double y, double radius) : Shape(x,y){
-		this->radius = radius;
+	Circle(double x, double y, double radius) : Shape(x,y), m_radius(radius){
 	}
-	double square() override
+	double square() const override
 	{
-		return pi * radius * radius;
+		return m_pi * m_radius * m_radius;
 	};
 };
 
 int main(){
-	Rectangle rectangle(0, 0, 5, 5);
-	Triangle triangle(0, 0, 5, 5);
-	Circle circle(0, 0, 5);
-	cout << rectangle.square() << endl;
-	cout << triangle.square() << endl;
-	cout << circle.square() << endl;
+	const int length = 3;
+	Shape* shapes[length];
+	shapes[0] = new Rectangle(0, 0, 5, 5);
+	shapes[1] = new Triangle(0, 0, 5, 5);
+	shapes[2] = new Circle(0, 0, 5);
+	for (size_t i = 0; i < length; i++)
+	{
+		cout << shapes[i]->square() << endl;
+	}
 	return 0;
 }
